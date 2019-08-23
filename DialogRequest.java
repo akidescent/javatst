41eb33a57414adfaaa39f68086c5e7ce7b953792
akie <student@akies-MBP.home>
2019-08-22 23:23:50 -0500

    try

---
+++ DialogRequest.java
@@ -0,0 +1,16 @@
+/*Akie Kasai
+*Harden
+*5th
+*DialogRequest.java
+*
+*/
+
+import javax.swing.JOptionPane;
+
+public class DialogRequest
+{
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Hello World!");
+	}
+}
