c0fdb2fa95e6afdef25e6c73e47719259560962b
akie <student@akies-MBP.home>
2019-08-22 23:35:47 -0500

    wwo

--- DialogRequest.java
+++ DialogRequest.java
@@ -2,7 +2,7 @@
 *Harden
 *5th
 *DialogRequest.java
-*
+*Asks user's name with message dialog boxes and input boxes.
 */
 
 import javax.swing.JOptionPane;
@@ -11,6 +11,9 @@ public class DialogRequest
 {
 	public static void main(String[] args)
 	{
-		JOptionPane.showMessageDialog(null, "Hello World!");
+		String name = JOptionPane.showInputDialog("What is your name?");
+		System.out.println("Hello!" + name);
+		String inputUser = JOptionPane.showInputDialog("What would you like me to do?");
+		JOptionPane.showMessageDialog("I'm sorry," + name + ". I'm afraid I can't do that.");
 	}
 }
